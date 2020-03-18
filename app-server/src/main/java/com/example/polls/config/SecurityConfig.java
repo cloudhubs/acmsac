ed8e73e4345e99ae6ef16d21e258cbff90e37683
Dipta Das <dipta670@gmail.com>
2020-03-17 23:52:42 -0500

    Permit all access to hello endpoint

--- app-server/src/main/java/com/example/polls/config/SecurityConfig.java
+++ app-server/src/main/java/com/example/polls/config/SecurityConfig.java
@@ -90,7 +90,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         .permitAll()
                     .antMatchers("/api/user/checkUsernameAvailability", "/api/user/checkEmailAvailability")
                         .permitAll()
-                    .antMatchers(HttpMethod.GET, "/api/polls/**", "/api/users/**")
+                    .antMatchers(HttpMethod.GET, "/api/polls/**", "/api/users/**", "/api/hello")
                         .permitAll()
                     .anyRequest()
                         .authenticated();
