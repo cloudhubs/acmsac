0e02c52c981d0a73b1a6897056112c851ac7591c
Dipta Das <dipta670@gmail.com>
2020-03-28 14:40:46 -0500

    Add html supported emails

--- app-server/src/main/java/com/example/polls/service/EmailService.java
+++ app-server/src/main/java/com/example/polls/service/EmailService.java
@@ -1,9 +1,11 @@
 package com.example.polls.service;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.internet.MimeMessage;
 
 @Service
 @Slf4j
@@ -14,13 +16,16 @@ public class EmailService {
         this.javaMailSender = javaMailSender;
     }
 
-    public void sendEmail(String from, String to, String subject, String content) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(from);
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(content);
-        log.info("Sending support email to " + to);
+    public void sendEmail(String from, String to, String subject, String content) throws Exception {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, "utf-8");
+
+        helper.setText(content, true);
+        helper.setFrom(from);
+        helper.setTo(to);
+        helper.setSubject(subject);
+
+        log.info("Sending email to " + to);
         javaMailSender.send(message);
     }
 }
